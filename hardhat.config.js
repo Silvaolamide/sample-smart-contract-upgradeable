0ce6ee59c76857bbf7505b5cb3122d6b773ecb4c
Olamide Silva Agunkejoye <Silvaolamide@gmail.com>
2022-09-22 11:34:52 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,9 @@ require("@nomiclabs/hardhat-etherscan");
 require("@openzeppelin/hardhat-upgrades");
 
 
-ALCHEMY_API_KEY2='xBfUwfpTLZ2xejtB4mjrsjrk0X7-jSoj';
-PRI_KEY2="71701ab2f5786ff1ad3b65ebea5d4f03d6e37d1409f77b9f99b3c32b30a530aa";
-ETHERSCAN_API_KEY='HITMNXU8VJ495828DNIFZSQWWX3QMECQIS';
+ALCHEMY_API_KEY2='';
+PRI_KEY2="";
+ETHERSCAN_API_KEY='';
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
@@ -14,7 +14,7 @@ module.exports = {
       goerli: {
         //   url: 'https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}',
         //   url: 'https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY2}',
-          url: 'https://goerli.infura.io/v3/57c00db1b90b40af9d0ba4bd9f0702d6',
+          url: 'https://goerli.infura.io/v3/${ALCHEMY_API_KEY2}',
           accounts: [PRI_KEY2],
       },
   },
